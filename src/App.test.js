eeafa44a912582c29492c817317dc3cb9f5fc311
Elliot <elliotrevan@gmail.com>
2021-04-12 10:16:18 +0100

    make it clear that the test is an example test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('example test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/some text here/i);
   expect(linkElement).toBeInTheDocument();
 });
